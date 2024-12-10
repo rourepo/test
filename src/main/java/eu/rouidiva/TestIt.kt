b00668d610b57ac907f154e57426a4b66cbf12e2
Alex <ca.radivoiu@gmail.com>
2024-12-10 18:32:44 +0200

    Init

---
+++ src/main/java/eu/rouidiva/TestIt.kt
@@ -0,0 +1,17 @@
+package eu.rouidiva
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun TestIt() {
+    Text("ohoho")
+}
+
+@Preview
+@Composable
+fun TestItPreview() {
+    TestIt()
+}
